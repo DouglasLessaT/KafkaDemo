83cb118fe2d6973c1417c70c24a20e5104e85bce
MAGOCIZENTO <douglaslessat@gmail.com>
2024-09-28 16:02:24 -0300

    atualização m1 e dockerfile

---
+++ m1sevicepayament/src/main/java/privateclasswizard/m1sevicepayament/model/Payment.java
@@ -0,0 +1,14 @@
+package privateclasswizard.m1sevicepayament.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+public class Payment {
+
+    private Long id;
+    private String orderId;
+    private double amount;
+}
